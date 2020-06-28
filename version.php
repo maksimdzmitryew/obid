1446a8950b34cc473539687a9e45f6fad7a47ce5
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2020-06-28 13:35:31 +0300

    0.00.2+a180:3 user ADD new version icon pack
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -2,11 +2,11 @@
 
 $a_production = [
     'version_release'       => '0',
-    'version_leader'        => '01', # changes whenever any css,js,api,guest,user value below has changed; reset each realease
-    'version_patch'         => '0',
+    'version_leader'        => '00', # changes whenever any css,js,api,guest,user value below has changed; reset each realease
+    'version_patch'         => '2',
     'version_maturity'      => 'a', # a=alfa,b=beta,rc=candidate,r=release,sr=service release
     'version_day'           => '180',
-    'version_seq'           => '1',
+    'version_seq'           => '3',
 ];
 
 return (object) [
@@ -20,9 +20,9 @@ return (object) [
     . $a_production['version_seq'],
     'release'               => $a_production['version_release'],
     'guest'                 => '0.00.0',
-    'user'                  => '0.01.0',
+    'user'                  => '0.00.2',
     'test'                  => '0.00.0',
     'api'                   => '0.00.0',
-    'css'                   => '0.06.0',
-    'js'                    => '0.04.0',
+    'css'                   => '0.08.0',
+    'js'                    => '0.06.0',
 ];
