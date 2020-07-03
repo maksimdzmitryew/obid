fa7cae39a729d18f3c3e4aadc295953bdad7c754
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2020-07-03 11:47:31 +0300

    0.00.2+a185:2 user ADD translation from parent model
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- app/Filters/FiltersAPI.php
+++ app/Filters/FiltersAPI.php
@@ -6,7 +6,7 @@ namespace App\Filters;
 use                                          DB;
 use                              App\Filters\Filters;
 use                          Illuminate\Http\Request;
-use                       Illuminate\Support\Pluralizer;
+use                       Illuminate\Support\Str;
 
 class FiltersAPI extends Filters
 {
@@ -37,7 +37,8 @@ class FiltersAPI extends Filters
 		{
 			$this->_env->s_name		= $a_tmp[1];
 			$this->_env->s_model	= '\Modules\\' . $this->_env->s_name . '\\' . $a_tmp[2] . '\\' . $this->_env->s_name ;
-			$this->_env->s_trans	= '\Modules\\' . $this->_env->s_name . '\\' . 'Database' . '\\' . $this->_env->s_name ;
+			$this->_env->s_main		= '\Modules\\' . $this->_env->s_name . '\\' . 'Database' . '\\' . $this->_env->s_name ;
+			$this->_env->s_trans	= $this->_env->s_main . 'Translation' ;
 		}
 		else
 		{
@@ -45,21 +46,66 @@ class FiltersAPI extends Filters
 			$this->_env->s_model	= '\App\\'.$this->_env->s_name;
 			$this->_env->s_trans	= $this->_env->s_model ;
 		}
-		$s_name_sgl = strtolower($this->_env->s_name);
-		$s_name_plr = Pluralizer::plural($s_name_sgl, 2);
-		$s_table_main = $s_name_plr;
-		$s_table_trans = $s_name_sgl.'_translations';
 
-		return $this->builder->select(
-				$s_table_main.'.*',
-				$s_table_trans.'.title as title'
-			)
-			->offset($this->request->start)
-			->limit($this->limit)
-			->leftJoin($s_table_trans, function($query) use ($s_name_sgl, $s_name_plr, $s_table_trans) {
-				$query->on($s_table_trans.'.'.$s_name_sgl.'_id', '=', $s_name_plr.'.id')
-					->where('locale', '=', $this->appLocale);
-			})
-			->orderBy($this->orderColumn, $this->orderDirection);
+		$o_query					= $this->builder;
+
+		$a_select					= [];
+		$s_tmp						= $this->_env->s_main;
+		if (class_exists($s_tmp))
+		{
+			$m						= new $s_tmp;
+			$a_fill_main			= $m->getFillable();
+			$a_form_main			= $m->getFields();
+			$s_table_main			= $m->getTable();
+			$a_select[]				= $s_table_main.'.*';
+			$s_parent_trans			= $m->translatedModel;
+		}
+
+		if (!is_null($s_parent_trans))
+		{
+			$s_tmp					= ucfirst($s_parent_trans);
+			$this->_env->s_trans	= '\Modules\\' . $s_tmp . '\\' . 'Database' . '\\' . $s_tmp . 'Translation' ;
+		}
+
+		$s_tmp						= $this->_env->s_trans;
+		if (class_exists($s_tmp))
+		{
+			$t						= new $s_tmp;
+			$a_fill_trans			= $t->getFillable();
+			$a_form_trans			= $t->getFields();
+			$s_table_trans			= $t->getTable();
+			$a_select[]				= $s_table_trans.'.title AS title';
+
+			if (!is_null($s_parent_trans))
+			{
+				$s_key_at			= $s_table_main;
+				$s_key_on			= $s_table_trans;
+				$s_name_sgl			= Str::singular($s_parent_trans);
+			}
+			else
+			{
+				$s_key_at			= $s_table_trans;
+				$s_key_on			= $s_table_main;
+				$s_name_sgl			= Str::singular($s_key_on);
+			}
+
+			$s_key_at				= $s_key_at.'.'.$s_name_sgl.'_id';
+			$s_key_on				= $s_key_on.'.id';
+
+			$o_query
+				->leftJoin($s_table_trans, function($query) use ($s_key_at, $s_key_on) {
+					$query->on($s_key_at, '=', $s_key_on)
+						->where('locale', '=', $this->appLocale);
+				});
+		}
+
+		$o_query
+				->select($a_select)
+				->offset($this->request->start)
+				->limit($this->limit)
+				->orderBy($this->orderColumn, $this->orderDirection)
+			;
+
+		return $o_query;
 	}
 }
--- version.php
+++ version.php
@@ -6,7 +6,7 @@ $a_production = [
     'version_patch'         => '2',
     'version_maturity'      => 'a', # a=alfa,b=beta,rc=candidate,r=release,sr=service release
     'version_day'           => '185',
-    'version_seq'           => '1',
+    'version_seq'           => '2',
 ];
 
 return (object) [
