95a6da0cbaccc7d71c65a30c45bb599e793be284
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2020-07-15 13:00:30 +0300

    0.02.1+a197:6 user ADD github and linkedin links
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- Modules/ObedyV1/Resources/views/layouts/_social_icons.blade.php
+++ Modules/ObedyV1/Resources/views/layouts/_social_icons.blade.php
@@ -1,5 +1,2 @@
-<a class="ico" target="_blank" href="https://www.facebook.com/Provodnik4u/"><img src="/{!! $theme !!}/img/soc_fb.png"  alt=""></a>
-<a class="ico" target="_blank" href="https://www.linkedin.com/company/provodnik/"><img src="/{!! $theme !!}/img/soc_li.png"  alt=""></a>
-<a class="ico" target="_blank" href="https://www.youtube.com/channel/UCCpqWOgNnsXJGiVn8uRITmQ/featured"><img src="/{!! $theme !!}/img/soc_yt.png"  alt=""></a>
-<a class="ico" target="_blank" href="https://www.instagram.com/provodnik4u/"><img src="/{!! $theme !!}/img/soc_in.png"  alt=""></a>
-<a class="ico" target="_blank" href="https://t.me/joinchat/BrZ7yFEfkm6bIoOE93U8dQ "><img src="/{!! $theme !!}/img/soc_tg.png"  alt=""></a>
+<a class="ico" target="_blank" href="https://github.com/maksimdzmitryew/obid"><img class="grayscale filter-green" src="https://github.githubassets.com/pinned-octocat.svg" alt="github" title="github"></a>
+<a class="ico" target="_blank" href="https://www.linkedin.com/in/maximax/"><img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" alt="linkedin" title="linkedin"></a>
