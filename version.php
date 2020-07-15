e14236cfd1864704c9cdd88094d91ce71f2283e4
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2020-07-15 11:02:09 +0300

    0.02.1+a197:3 user ADD make past not selected items less visible in week schedule
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -6,7 +6,7 @@ $a_production = [
     'version_patch'         => '1',
     'version_maturity'      => 'a', # a=alfa,b=beta,rc=candidate,r=release,sr=service release
     'version_day'           => '197',
-    'version_seq'           => '2',
+    'version_seq'           => '3',
 ];
 
 return (object) [
@@ -23,6 +23,6 @@ return (object) [
     'user'                  => '0.02.1',
     'test'                  => '0.00.0',
     'api'                   => '0.00.0',
-    'css'                   => '0.23.0',
+    'css'                   => '0.24.0',
     'js'                    => '0.12.0',
 ];
