c78416632b0838030f4dc7b14ca2699c938e7087
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2020-07-18 02:27:29 +0300

    0.02.2+a200:2 user ADD fancy radiobutton, see security options at signin page
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -6,7 +6,7 @@ $a_production = [
     'version_patch'         => '2',
     'version_maturity'      => 'a', # a=alfa,b=beta,rc=candidate,r=release,sr=service release
     'version_day'           => '200',
-    'version_seq'           => '1',
+    'version_seq'           => '2',
 ];
 
 return (object) [
@@ -23,6 +23,6 @@ return (object) [
     'user'                  => '0.02.2',
     'test'                  => '0.00.0',
     'api'                   => '0.00.0',
-    'css'                   => '0.27.0',
+    'css'                   => '0.28.0',
     'js'                    => '0.13.0',
 ];
