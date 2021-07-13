7a966ced7675c3183dcc27a975c13d2705c0050d
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-07-14 00:36:57 +0300

    0.05.1+a195:2 user FIX migration missing index
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- database/migrations/2021_07_13_000000_create_users_table.php
+++ database/migrations/2021_07_13_000000_create_users_table.php
@@ -17,15 +17,16 @@ class CreateUsersTable extends Migration
 	{
 		Schema::connection(self::DB_CONNECTION)->create(self::TABLE_MIGRATION, function (Blueprint $table) {
 			$table->increments('id');
-			$table->boolean('enabled')										->default(0)->nullable(false)->comment('account is enabled and user will be allowed to sign in');
-			$table->string('email')						->unique()							->nullable(false);
+			$table->boolean('enabled')										->default(0)->nullable(false)->index()->comment('account is enabled and user will be allowed to sign in');
+			$table->string('email')						->unique()							->nullable(false)->index();
 			$table->string('password')																->nullable(false);
 			$table->string('title')																		->nullable(false);
-			$table->string('first_name')															->nullable(false);
+			$table->string('first_name')															->nullable(false)->index();
 			$table->string('last_name')																->nullable(false);
 			$table->rememberToken()																		->nullable(false);
-			$table->string('activation_token', 40)										->nullable(false);
+			$table->string('activation_token', 40)										->nullable(false)->index();
 			$table->timestamps();
+			$table->index(['last_name', 'first_name'], self::TABLE_MIGRATION . '_name_index');
 		});
 	}
 
