35957a145becf017a9db962735530531938470ad
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-07-15 00:49:38 +0300

    0.11.0+a196:1 test ADD middleware TrimStrings
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

---
+++ tests/Unit/TrimStringsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit;
+
+use                                       Tests\TestCase;
+use                         App\Http\Middleware\TrimStrings as Middleware;
+use                             Illuminate\Http\Request;
+
+class TrimStringsTest extends TestCase
+{
+
+		protected $s_sample	= '  String Surrounded by Spaces  ';
+
+		public function testDoTransform()
+		{
+				$request = new Request;
+
+				$request->merge([
+						'last_name' => $this->s_sample
+				]);
+
+				$middleware = new Middleware;
+
+				$middleware->handle($request, function ($req) {
+						$this->assertEquals(trim($this->s_sample), $req->last_name);
+				});
+		}
+
+		public function testSkipTransform()
+		{
+				$request = new Request;
+
+				$request->merge([
+						'password' => $this->s_sample
+				]);
+
+				$middleware = new Middleware;
+
+				$middleware->handle($request, function ($req) {
+						$this->assertEquals($this->s_sample, $req->password);
+				});
+		}
+
+}
