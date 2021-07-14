9f120bad0017b103e4d00e164c67f7e46e19c27e
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-07-14 22:46:51 +0300

    0.11.0+a195:6 test ADD check guest and power pages
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- tests/Feature/FeatureTest.php
+++ tests/Feature/FeatureTest.php
@@ -11,9 +11,30 @@ class FeatureTest extends TestCase
      *
      * @return void
      */
-    public function testHome()
+    public function testGuest()
     {
+        /**
+         *  errors will be written to stdout instead of to page contents
+         */
+        $this->withoutExceptionHandling();
+
         $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testPower()
+    {
+        /**
+         *  errors will be written to stdout instead of to page contents
+         */
+        $this->withoutExceptionHandling();
+
+        $response = $this->get('/signin');
         $response->assertStatus(200);
     }
 }
