743ca6f5b302ea3b1d55e382773bd1c9ff27473a
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-07-16 00:18:39 +0300

    0.11.5+a197:1 test FIX actual log branch for commits
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 $a_production = [
     'version_release'       => '0',
     'version_leader'        => '11', # changes whenever any css,js,api,guest,user value below has changed; reset each realease
-    'version_patch'         => '3',
+    'version_patch'         => '5',
     'version_maturity'      => 'a', # a=alfa,b=beta,rc=candidate,r=release,sr=service release
     'version_day'           => '197',
     'version_seq'           => '1', # subsequent commit during same day for the same version
@@ -21,7 +21,7 @@ return (object) [
     'release'               => $a_production['version_release'],
     'guest'                 => '0.00.0',
     'user'                  => '0.10.2',
-    'test'                  => '0.01.1',
+    'test'                  => '0.01.3',
     'api'                   => '0.00.0',
     'css'                   => '0.32.0',
     'js'                    => '0.24.0',
