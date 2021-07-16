56c2958c549af0c57b486c595ab43b86e5a86a57
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-07-16 10:29:42 +0300

    0.11.6+a197:1 test FIX model t e s t namespace
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -3,10 +3,10 @@
 $a_production = [
     'version_release'       => '0',
     'version_leader'        => '11', # changes whenever any css,js,api,guest,user value below has changed; reset each realease
-    'version_patch'         => '5',
+    'version_patch'         => '6',
     'version_maturity'      => 'a', # a=alfa,b=beta,rc=candidate,r=release,sr=service release
     'version_day'           => '197',
-    'version_seq'           => '2', # subsequent commit during same day for the same version
+    'version_seq'           => '1', # subsequent commit during same day for the same version
 ];
 
 return (object) [
@@ -21,7 +21,7 @@ return (object) [
     'release'               => $a_production['version_release'],
     'guest'                 => '0.00.0',
     'user'                  => '0.10.2',
-    'test'                  => '0.01.3',
+    'test'                  => '0.01.4',
     'api'                   => '0.00.0',
     'css'                   => '0.32.0',
     'js'                    => '0.25.0',
