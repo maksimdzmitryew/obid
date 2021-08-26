624ebe86add16e6760b5b228a5e33f0bcfe9c7bc
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-08-26 16:35:13 +0300

    0.13.0+a238:3 guest ADD unified translation migration creation
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

---
+++ database/migrations/2021_07_11_030303_create_cities_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use               Illuminate\Database\Schema\Blueprint;
+use           Illuminate\Database\Migrations\Migration;
+use                               App\Traits\MigrationTrait;
+use               Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration
+{
+    use MigrationTrait;
+
+    const DB_CONNECTION         = 'psc';
+    const TABLE_MIGRATION       = 'cities';
+
+    protected static $o_country = '';
+
+    public function __construct()
+    {
+        self::$o_country        = new CreateCountriesTable();
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection($this->getConnection())->create('cities', function (Blueprint $table) {
+            $this->addPrimaryKey($this, $table);
+            $this->addForeignKey(self::$o_country, $table);
+
+            $table->string('timezone');
+            $table->timestamps();
+        });
+        $this->upTranslationMigration();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->downTranslationMigration();
+        Schema::connection($this->getConnection())->dropIfExists('cities');
+    }
+}
