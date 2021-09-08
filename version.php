8f42f75ccfde3b012afbbb36453ccbef7e867b90
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-09-08 12:49:55 +0300

    0.13.0+a251:3 guest ADD clean legacy values in settings
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -6,7 +6,7 @@ $a_production = [
     'version_patch'         => '0',
     'version_maturity'      => 'a', # a=alfa,b=beta,rc=candidate,r=release,sr=service release
     'version_day'           => '251',
-    'version_seq'           => '2', # subsequent commit during same day for the same version
+    'version_seq'           => '3', # subsequent commit during same day for the same version
 ];
 
 return (object) [
@@ -24,5 +24,5 @@ return (object) [
     'test'                  => '0.01.0',
     'api'                   => '0.00.0',
     'css'                   => '0.38.0',
-    'js'                    => '0.27.0',
+    'js'                    => '0.28.0',
 ];
