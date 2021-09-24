86a5a85c3b16097fdfaa85a7c2e86fa673c93b3e
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-09-24 13:37:21 +0300

    0.13.3+a267:2 anonym ADD social icons from settings instead of hard-coded
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- Modules/ObedyV1/Resources/views/layouts/_social_icons.blade.php
+++ Modules/ObedyV1/Resources/views/layouts/_social_icons.blade.php
@@ -1,2 +1,6 @@
-<a class="ico" target="_blank" href="https://github.com/maksimdzmitryew/obid"><img class="grayscale filter-green" src="https://github.githubassets.com/pinned-octocat.svg" alt="github" title="github"></a>
-<a class="ico" target="_blank" href="https://www.linkedin.com/in/maximax/"><img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" alt="linkedin" title="linkedin"></a>
+@if (!empty($settings->github))
+<a class="ico" target="_blank" href="{{ $settings->github }}"><img class="grayscale filter-green" src="https://github.githubassets.com/pinned-octocat.svg" alt="github" title="github"></a>
+@endif
+@if (!empty($settings->linkedin))
+<a class="ico" target="_blank" href="{{ $settings->linkedin }}"><img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" alt="linkedin" title="linkedin"></a>
+@endif
