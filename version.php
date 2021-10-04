c1219e8519bb9226c8edf27466ab14b122aa74ef
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-10-04 17:39:12 +0300

    0.15.0+a277:1 person DEL legacy field properties
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 $a_production = [
     'version_release'       => '0',
-    'version_leader'        => '14', # changes whenever any css,js,api,anonym,person value below has changed; reset each realease
+    'version_leader'        => '15', # changes whenever any css,js,api,anonym,person value below has changed; reset each realease
     'version_patch'         => '0',
     'version_maturity'      => 'a', # a=alfa,b=beta,rc=candidate,r=release,sr=service release
     'version_day'           => '277',
@@ -20,7 +20,7 @@ return (object) [
     . $a_production['version_seq'],
     'release'               => $a_production['version_release'],
     'anonym'                => '0.02.0',
-    'person'                => '0.11.0',
+    'person'                => '0.12.0',
     'review'                => '0.01.0',
     'api'                   => '0.00.0',
     'css'                   => '0.40.0',
