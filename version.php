3b09a8c3b5a590e9bafc9bad51826378c01c229b
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-10-04 17:37:16 +0300

    0.14.0+a277:1 anonym ADD change tabs into spaces for PSR-4
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -5,7 +5,7 @@ $a_production = [
     'version_leader'        => '14', # changes whenever any css,js,api,anonym,person value below has changed; reset each realease
     'version_patch'         => '0',
     'version_maturity'      => 'a', # a=alfa,b=beta,rc=candidate,r=release,sr=service release
-    'version_day'           => '274',
+    'version_day'           => '277',
     'version_seq'           => '1', # subsequent commit during same day for the same version
 ];
 
