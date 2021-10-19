f414965af6ebec28d1ebfe071056cb5c5be956d5
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-10-19 14:33:21 +0300

    0.18.0+a292:1 anonym DEL legacy views for non-modules
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -2,11 +2,11 @@
 
 $a_production = [
     'version_release'       => '0',
-    'version_leader'        => '17', # changes whenever any css,js,api,anonym,person value below has changed; reset each realease
+    'version_leader'        => '18', # changes whenever any css,js,api,anonym,person value below has changed; reset each realease
     'version_patch'         => '0',
     'version_maturity'      => 'a', # a=alfa,b=beta,rc=candidate,r=release,sr=service release
     'version_day'           => '292',
-    'version_seq'           => '2', # subsequent commit during same day for the same version
+    'version_seq'           => '1', # subsequent commit during same day for the same version
 ];
 
 return (object) [
@@ -19,7 +19,7 @@ return (object) [
     . $a_production['version_day'] . ':'
     . $a_production['version_seq'],
     'release'               => $a_production['version_release'],
-    'anonym'                => '0.02.0',
+    'anonym'                => '0.03.0',
     'person'                => '0.14.0',
     'review'                => '0.01.0',
     'api'                   => '0.00.0',
