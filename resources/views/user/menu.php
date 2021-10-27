5d189126b7f218fbd05f8cda946f01c5978aee8f
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-10-27 15:02:19 +0300

    0.18.4+a300:2 person ADD clean legacy settings for power menu
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- resources/views/user/menu.php
+++ resources/views/user/menu.php
@@ -4,15 +4,7 @@ $menu_color			= [];
 $text_color			= [];
 $menu_icon			= [];
 $menu_list			= [];
-/*
-$menu_color = '#f5ad42';
-$menu_color = '#ab2b67';
-$menu_icon = 'fa fa-sign-language';
-$menu_list = ['festivals' => 'icon-music', 'partners' => 'fa fa-handshake-o', 'events' => 'icon-calendar', 'presses' => 'icon-newspaper', 'news' => 'fa fa-globe'];
-$menu_list = ['categories' => 'icon-list2', 'places' => 'icon-pin-alt', 'cities' => 'icon-city', 'vocations' => 'icon-headset', 'professions' => 'fa fa-briefcase'];
-$menu_list[] = ['settings', 'texts', ];
-$menu_list = ['settings' => 'fa fa-cog', 'texts' => 'icon-file-text2', 'pages' => 'icon-versions'];
-*/
+
 $menu_title[]		= 'foods';
 $menu_color[]		= '#5933d6';
 $text_color[]		= '#DDD';
@@ -25,7 +17,6 @@ $menu_color[]		= '#f5ad42';
 $text_color[]		= '#555';
 $menu_icon[]		= 'fas fa-cash-register';
 $menu_list[]		= ['demand', ];
-#$menu_list[] = ['report', 'issue', ];
 
 $menu_title[]		= 'people';
 $menu_color[]		= '#33d669';
