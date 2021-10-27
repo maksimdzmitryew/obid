6652a0b443570769f5edf1cd28d08e75732da2df
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-10-27 14:57:52 +0300

    0.18.2+a300:1 anonym FIX missing view for confidentiality info
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

---
+++ Modules/ObedyV1/Resources/views/guest/_confidentiality_info.blade.php
@@ -0,0 +1,7 @@
+                <p class="confidentiality">
+                    @if ($id == 'signup')
+                        я погоджуюсь з <a href="/info/confidentiality" target="_blank">Правилами та Політикою конфіденційності Provodnik</a>
+                    @elseif ($id == 'footer')
+                        Використовуючи сайт Provodnik, ви автоматично приймаєте <a href="/info/confidentiality" target="_blank">Правила та Політику конфіденційності Provodnik</a>
+                    @endif
+                </p>
