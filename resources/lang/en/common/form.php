f6ca7252d57cb19f659113db33a9dc9f9f040d0e
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-11-10 16:55:42 +0200

    0.19.2+a314:3 person ADD more tranklations to Ukrainian
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

---
+++ resources/lang/en/common/form.php
@@ -0,0 +1,35 @@
+<?php
+return [
+	'breadcrumbs' => [
+		'list' => 'Список',
+	],
+	'text' => [
+		'create'		=> 'Створити',
+		'create_more'	=> 'Створити ще один запис',
+		'evaluate'		=> 'Оцініти цій запис',
+		'edit'			=> 'Редагування',
+		'view'			=> 'Перегляд',
+	],
+	'actions' => [
+		'continue'		=> 'Продовжити',
+		'create'		=> 'Створити',
+		'create_more'	=> 'Створити ще',
+		'edit'			=> 'Редагувати',
+		'view'			=> 'Подивитись',
+	],
+	'fields' => [
+		'published' => [
+			'label' => 'Active',
+		],
+//
+// TODO remove when users Module is ready
+/********************************* datatable *********************************/
+		'active' => [
+			'label' => 'Активовано',
+		],
+/********************************* /datatable *********************************/
+// TODO remove when users Module is ready
+//
+	],
+
+];
---
+++ resources/lang/en/common/messages.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+	'apply_filters' => 'filters have been applied',
+	'reset_filters' => 'filters have been reset',
+	'order_entries_success' => ':qty записів було впорядковано',
+	'order_entries_error' => 'Під час упорядкування сталася помилка',
+	'order_choose_parent' => 'Спочатку застосуйте фільтр «Батьківськая» сторінка',
+	'order_qty_mistmatch' => 'Кількість не відповідає. Варто оновіть сторінку',
+
+	'delete_entries_success' => 'Entries were successfully deleted',
+	'delete_entries_error' => 'An error occurred while deleting records',
+	'delete_entries_cancelled' => 'Cancelled deletion',
+	'confirm_delete_title' => 'Are you sure?',
+	'confirm_delete_description' => 'You will not be able to recover the record(s)',
+	'confirm_delete_confirm_button_text' => 'Yes, delete',
+	'confirm_delete_cancel_button_text' => 'Cancel',
+	'json_parse_error' => 'Помилкова json-відповідь',
+	'email' => [
+		'subject' => 'Confirm registration on :app_name',
+		'confirm' => [
+			'title' => 'Confirm your email address on :app_name',
+			'open' => 'Hello! We just need to verify that',
+			'close' => 'is your email address, and then we’ll help you login to you personal :app_name cabinet.',
+			'link' => 'Confirm Email Address',
+			'thanks' => 'Thanks for signing up to :app_name.',
+		],
+		'disclaimer' => [
+			'title' => 'Didn’t request this email?',
+			'text' => 'No worries! Your address may have been entered by mistake. If you ignore or delete this email, nothing further will happen.',
+		],
+	],
+];
