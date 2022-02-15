487ba7ce47802a64ff4cbc1ec10131fd80b2b390
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2022-02-15 20:15:35 +0200

    0.20.0+a046:1 anonym DEL city and country legacy code
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -2,10 +2,10 @@
 
 $a_production = [
     'version_release'       => '0',
-    'version_leader'        => '19', # changes whenever any css,js,api,anonym,person value below has changed; reset each realease
-    'version_patch'         => '3',
+    'version_leader'        => '20', # changes whenever any css,js,api,anonym,person value below has changed; reset each realease
+    'version_patch'         => '0',
     'version_maturity'      => 'a', # a=alfa,b=beta,rc=candidate,r=release,sr=service release
-    'version_day'           => '314',
+    'version_day'           => '046',
     'version_seq'           => '1', # subsequent commit during same day for the same version
 ];
 
@@ -19,7 +19,7 @@ return (object) [
     . $a_production['version_day'] . ':'
     . $a_production['version_seq'],
     'release'               => $a_production['version_release'],
-    'anonym'                => '0.04.3',
+    'anonym'                => '0.05.0',
     'person'                => '0.14.0',
     'review'                => '0.01.0',
     'api'                   => '0.00.0',
